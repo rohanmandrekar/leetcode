2d0dac9e3eb95d4447dfcb006221ca6beb122586
rohanmandrekar <rmaskaban@gmail.com>
2022-06-27 15:53:52 -0500

    Update 6. ZigZag Conversion.py

--- 6. ZigZag Conversion.py
+++ 6. ZigZag Conversion.py
@@ -1,23 +1,28 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
-        if numRows==1 or numRows>=len(s):
+        if numRows==1 or len(s)<=numRows:
             return s
-        
+        ans=''
         r=[[] for num in range(0,numRows)]
+        i=0
         flag=-1
-        row=0
-        
-        for char in s:
-            r[row].append(char)
-            if row==0 or row==numRows-1:
+        for letter in s:           
+            r[i].append(letter)
+            if i==0 or i==numRows-1 :
                 flag=flag*(-1)
-            row+=flag
-        ans=''    
+                
+            if flag==-1:
+                i-=1
+            else:
+                i+=1
+
         for row in r:
-            for char in row:
-                ans+=char            
-        return ans
+            for val in row:
+                ans+=val
+        return ans    
+            
+            
                 
             
             
