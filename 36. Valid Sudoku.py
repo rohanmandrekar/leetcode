b84f7a93762bd8e8c083a3fb49bc3402134981ec
rohanmandrekar <rmaskaban@gmail.com>
2022-07-16 19:46:10 -0500

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -1,19 +1,19 @@
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols=collections.defaultdict(set)
-        rows=collections.defaultdict(set)
-        squares=collections.defaultdict(set)
+        row=collections.defaultdict(set)
+        col=collections.defaultdict(set)
+        block=collections.defaultdict(set)
         
         for r in range(9):
             for c in range(9):
-                if board[r][c]==".":
+                if board[r][c]=='.':
                     continue
-                if (board[r][c] in cols[c] or 
-                    board[r][c] in rows[r] or 
-                    board[r][c] in squares[(r//3,c//3)]):
-                    return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])
-        return True        
+                else:
+                    if board[r][c] in row[r] or board[r][c] in col[c] or board[r][c] in block[r//3,c//3]:
+                        return False
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                block[r//3,c//3].add(board[r][c])
+        
+        return True
