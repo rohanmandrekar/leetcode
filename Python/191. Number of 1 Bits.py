8d6e2b66ea2d85ea6fea4552516071b9fdfc522d
rohanmandrekar <rmaskaban@gmail.com>
2023-06-26 13:08:22 -0500

    Update 191. Number of 1 Bits.py

--- Python/191. Number of 1 Bits.py
+++ Python/191. Number of 1 Bits.py
@@ -1,8 +1,14 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         
-        c=0
+        # c=0
+        # while n:
+        #     n=n&(n-1)
+        #     c+=1
+        # return c    
+        
+        ans=0
         while n:
-            n=n&(n-1)
-            c+=1
-        return c
+            ans+=n%2
+            n=n>>1
+        return ans    
