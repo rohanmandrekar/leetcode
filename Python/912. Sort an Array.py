df1509b7d2656ad24ebeb3038d157f048f4f3a9f
rohanmandrekar <rmaskaban@gmail.com>
2023-08-10 20:41:11 -0500

    Update 912. Sort an Array.py

--- Python/912. Sort an Array.py
+++ Python/912. Sort an Array.py
@@ -1,9 +1,10 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-
         def merge(arr,L,M,R):
-            left,right=arr[L:M+1],arr[M+1:R+1]
-            i,j,k=L,0,0
+            i=L
+            j=k=0
+            left=arr[L:M+1]
+            right=arr[M+1:R+1]
 
             while j<len(left) and k<len(right):
                 if left[j]<=right[k]:
@@ -13,25 +14,25 @@ class Solution:
                     arr[i]=right[k]
                     k+=1
                 i+=1
+
             while j<len(left):
                 arr[i]=left[j]
                 j+=1
                 i+=1
+
             while k<len(right):
                 arr[i]=right[k]
                 k+=1
                 i+=1
-            return arr                    
-
-        
-        def mergesort(arr,l,r):
+                            
+        def mergeSort(arr,l,r):
             if l==r:
                 return arr
+            m=(l+r)//2      
 
-            m=(l+r)//2
-            mergesort(arr,l,m)
-            mergesort(arr,m+1,r)
+            mergeSort(arr,l,m)
+            mergeSort(arr,m+1,r)
             merge(arr,l,m,r)
             return arr
 
-        return mergesort(nums,0,len(nums)-1)  
+        return mergeSort(nums,0,len(nums)-1)   
