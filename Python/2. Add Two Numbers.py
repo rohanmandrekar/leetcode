b17b17de38a7acc7025acbee115b244b75890f81
rohanmandrekar <rmaskaban@gmail.com>
2023-11-01 21:34:16 -0500

    Update 2. Add Two Numbers.py

--- Python/2. Add Two Numbers.py
+++ Python/2. Add Two Numbers.py
@@ -5,23 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
-        carry=0
         s=0
-        dummy=ListNode()
-        
-        current=dummy
-        
+        carry=0
+        dummy = ListNode()
+
+        current = dummy
+
         while l1 or l2 or carry:
             s=carry
             if l1:
-                s+=l1.val  
+                s+=l1.val
                 l1=l1.next
             if l2:
                 s+=l2.val
-                l2=l2.next
+                l2=l2.next 
             carry=int(s/10)
             current.next=ListNode(s%10)
             current=current.next
-        return dummy.next    
-            
+
+        return dummy.next       
