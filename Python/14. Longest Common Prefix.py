b38ca21a5b65dc8dd60f02ab0acad0903952077f
rohanmandrekar <rmaskaban@gmail.com>
2023-11-26 23:34:20 -0600

    Update 14. Longest Common Prefix.py

--- Python/14. Longest Common Prefix.py
+++ Python/14. Longest Common Prefix.py
@@ -2,24 +2,9 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         ans=''
         
-        l=999
-        
-        for word in strs:
-            if len(word)<l:
-                l=len(word)
-        
-        for i in range(0,l):
-            flag=0
-            x=strs[0]
-            y=x[i]
-            
-            for word in strs:
-                if word[i]==y:
-                    flag+=1
-            if flag==len(strs):
-                ans+=y
-            elif flag<len(strs):
-                break
-                
-        return ans       
-                
+        for i in range (len(strs[0])):
+            for s in strs:
+                if i==len(s)  or s[i]!=strs[0][i]:
+                    return ans
+            ans+=s[i]
+        return ans
