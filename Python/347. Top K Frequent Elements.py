52efad9b6c647d7f04e3fe1222449ca72110e159
rohanmandrekar <rmaskaban@gmail.com>
2024-01-16 16:07:59 -0600

    Update 347. Top K Frequent Elements.py

--- Python/347. Top K Frequent Elements.py
+++ Python/347. Top K Frequent Elements.py
@@ -12,20 +12,27 @@
 ##            del dic[max(dic,key=dic.get)]
 ##        return ans   
 
+##class Solution:
+##    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+##        dic={}
+##        ans=[]
+##        freq=[[] for i in range(len(nums)+1)]
+##        for num in nums:
+##            dic[num]=1+dic.get(num,0)
+##        for n,c in dic.items():
+##            freq[c].append(n)
+##
+##        for i in range(len(nums),0,-1):
+##            for val in freq[i]:
+##                ans.append(val)
+##                if len(ans)==k:
+##                    return ans        
+
+           
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dic={}
-        ans=[]
-        freq=[[] for i in range(len(nums)+1)]
-        for num in nums:
-            dic[num]=1+dic.get(num,0)
-        for n,c in dic.items():
-            freq[c].append(n)
-
-        for i in range(len(nums),0,-1):
-            for val in freq[i]:
-                ans.append(val)
-                if len(ans)==k:
-                    return ans        
-
-           
+        for n in nums:
+            dic[n]=1+dic.get(n,0)  
+        ans=dict(sorted(dic.items(), key=lambda item: item[1])[-k:])
+        return ans.keys()
