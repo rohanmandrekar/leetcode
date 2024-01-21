81d44b76bc914097d24203be58c991ebe6c2b924
rohanmandrekar <rmaskaban@gmail.com>
2024-01-20 18:42:10 -0600

    Update 20. Valid Parentheses.py

--- Python/20. Valid Parentheses.py
+++ Python/20. Valid Parentheses.py
@@ -1,20 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s)%2==1:
-            return False
         stack=[]
         dic={')':'(',']':'[','}':'{'}
         for val in s:
-            if (val=='{' or val=='[' or val=="("):
-                
+            if val not in dic:
                 stack.append(val)
-            if (val=="}" or val ==')' or val==']'):
-                if not stack:
-                    return False
-                if stack and stack[-1]!=dic[val]:
-                    return False
-                elif stack and stack[-1]==dic[val] :
-                    stack.pop()
-       
+            elif not stack or dic[val]!=stack.pop():
+                return False    
         return not stack
                     
