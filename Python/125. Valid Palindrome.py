45cb5a8fdb80ef4576790cc5aac09403e1792cf0
Rohan Mandrekar <rmaskaban@gmail.com>
2024-03-15 16:12:39 -0500

    Update 125. Valid Palindrome.py

--- Python/125. Valid Palindrome.py
+++ Python/125. Valid Palindrome.py
@@ -1,23 +1,9 @@
 class Solution:
-    def alphanum(self,c):
-        return (ord('A')<=ord(c)<=ord('Z') or ord('a')<=ord(c)<=ord('z') or ord('0')<=ord(c)<=ord('9'))
-    
     def isPalindrome(self, s: str) -> bool:
-        l=0
-        r=len(s)-1
-        
-        while l<r:
-            while l<r and not self.alphanum(s[l]):
-                l+=1
-                
-            while l<r and not self.alphanum(s[r]):
-                r-=1
-                
-            if s[l].lower()!=s[r].lower():
-                return False
-            l+=1
-            r-=1
-            
-        return True   
+        a=[]
+        for val in s:
+            if val.isalnum():
+                a.append(val.lower())
+        return a==a[::-1]           
             
         
